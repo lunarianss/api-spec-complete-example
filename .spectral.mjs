d82c50d0146dc2c1844b44820238c7969f768a56
cyan@apifox.com <cyan@apifox.com>
2026-03-27 18:00:24 +0800

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwODM2MA/spectral.js?branch_slug=main&token=8f40fe80-0db2-47ad-9dc5-00492d64bd6a";
+export default { extends: ruleset };
